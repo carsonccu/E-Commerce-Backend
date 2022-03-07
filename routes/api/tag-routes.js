2a3af8211c0b41f3116558391644ad64ebd31743
Carson <carsoncolgateccu@gmail.com>
2022-03-07 13:49:35 -0700

    finished my get routes for tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,9 +6,29 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tag = await Tag.findAll({ include: [{ model: Product, as: "tag_to_product" }] });
+    res.status(200).json(tag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
+  try {
+    const tag = await Tag.findByPk(req.params.id, { include: [{ model: Product, as: "tag_to_product" }] });
+
+
+    if (!tag) {
+      res.status(404).json({ message: 'No Tag with this ID, please try enter a valid ID' });
+      return;
+    }
+
+
+    res.status(200).json(tag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
